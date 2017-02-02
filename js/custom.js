06c72e9d12df1345c0748ae123c44ff2d15168a7
Rubin Thomas <rubin.thomas@stonybrook.edu>
2017-02-01 23:59:09 -0500

    added preloader

--- js/custom.js
+++ js/custom.js
@@ -1,28 +1,38 @@
 
 function makeClear(){
-	 document.getElementById("bg").style.opacity = 1; 
 	 setTimeout(function(){
 	 document.getElementById("frontscreen").style.opacity = 1;
 	 }, 800);
 	 setTimeout(function(){
 	 document.getElementById("fb").style.opacity = 1;
-	 }, 1200);
+	 }, 1600);
 	 setTimeout(function(){
 	 	document.getElementById("ig").style.opacity = 1;
-	 }, 1700);
+	 }, 2100);
 	 setTimeout(function(){
 	 	document.getElementById("gh").style.opacity = 1;
-	 }, 2200);
+	 }, 2600);
 	 setTimeout(function(){
 	 	document.getElementById("res").style.opacity = 1;
-	 }, 2700);
+	 }, 3100);
 }
 
 //SETS TIME OUT FOR FADE IN OF BACKGROUND PICTURE, HI, and BOXES.
 function begin(){
+	setTimeout(function (){
+			document.getElementById("status").style.opacity = 0;
+		    document.getElementById("preloader").style.opacity = 0;
+	}, 800);
 	setTimeout(function() { makeClear() }, 900);
     //setTimeout(function() { showIcons() }, 1800)
 }
 
+
 //WHEN THE DOCS READY, START EXECUTING THIS ANIMATION CODE.
+
+
 $(document).ready(begin)
+
+
+
+
